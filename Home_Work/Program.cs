c58440385449ccd055819a1efc735cb6bade4f15
Lilith <ll.file.top.secret@yandex.ru>
2023-02-04 23:55:23 +0300

    add Program.cs + README.md

---
+++ Home_Work/Program.cs
@@ -0,0 +1,26 @@
+﻿using static Common.Helper;
+
+// Написать программу, которая из имеющегося массива строк 
+// формирует массив из строк, длина которых меньше либо равна 3 символам.
+
+string[] initialArray = new string[] {"hello", "2", "world", ":-)", "1234", "1567", "-2"};
+PrintArray(initialArray);
+
+string[] newArr = new string[initialArray.Length];
+
+GenerateThreeDigArr(initialArray, newArr);
+PrintArray(newArr);
+
+void GenerateThreeDigArr(string[] initialArray, string[] newArr)
+{
+    int count = 0;
+    for (int i = 0; i < initialArray.Length; i++)
+    {
+    if(initialArray[i].Length <= 3)
+        {
+        newArr[count] = initialArray[i];
+        count++;
+        }
+    }
+}
+
